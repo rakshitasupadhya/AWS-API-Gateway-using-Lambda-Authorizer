ab8a5092bdfc01ab6b75af784d8a9ab871de8268
Rakshitha <107621546+rakshitasupadhya@users.noreply.github.com>
2023-10-29 19:09:25 +0530

    Create lambda_authorizer.py

---
+++ lambda_authorizer.py
@@ -0,0 +1,15 @@
+def lambda_handler(event, context):
+    
+    #1 - Log the event
+    print('*********** The event is: ***************')
+    print(event)
+    
+    #2 - See if the person's token is valid
+    if event['authorizationToken'] == 'abc123':
+        auth = 'Allow'
+    else:
+        auth = 'Deny'
+    
+    #3 - Construct and return the response
+    authResponse = { "principalId": "abc123", "policyDocument": { "Version": "2012-10-17", "Statement": [{"Action": "execute-api:Invoke", "Resource": ["arn:aws:execute-api:us-east-1:787378350548:offdmk363a/*/*"], "Effect": auth}] }}
+    return authResponse
